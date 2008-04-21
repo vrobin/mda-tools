eb104272f18b32f41ae1d38f42ef2ecd1aff92ef
vrobin <vrobin@288f6e11-c249-0410-9ace-c9b9246f531a>
2008-04-21 23:03:06 +0000

    At least, some directory content reading is written, but unfinished.
    Created an "_addFolder" method, but it has to be finished (handling windows drives, escaping dot in extension check).
    
    an empty _createFolderItem has been created, it should be used to populate a "folderItem". To be continued...

--- MDAPlayground/DirExplorerTree.pm
+++ MDAPlayground/DirExplorerTree.pm
@@ -7,10 +7,14 @@ use strict;
 use utf8;
 
 use Data::Dumper;
+#use File::Find;
+#use File::Next;
 use File::Spec;
+#use File::Util;
 use Log::Log4perl qw(:easy);
 use Win32API::File;
 
+my $mediaExtensions = [ '.cue', '.ape', '.wav', '.flac', '.mp3', '.wv', 'aiff'];
 my $windows = 0;
 if ($^O =~ /^m?s?win/i) {
 	$windows = 1;
@@ -50,15 +54,28 @@ sub init {
 	$tree->configure(-treecolumn => 'folderTag');
 	my $column2 = $tree->column("create", -text=>"âàèé");
 	
+	$tree->state("define", "hasSubFolders");
+	$tree->state("define", "hasMediaFile");
+	$tree->state("define", "hasMdaXmlFile");
+
+    $tree->element("create", "elemImg", "image",  -image => [
+	$mdaAwarePicto, ["hasMdaXmlFile"],
+	$musicPicto,  ["hasMediaFile", "!hasMdaXmlFile"],
+	$folderPicto, []
+	]
+	);
+    
 	$tree->element("create", "myComputerFolderImage", "image", -image => $myComputerPicto);
 	$tree->element("create", "classicFolderImage", "image", -image => $folderPicto);
 	$tree->element("create", "folderTxt", "text");
 	
+	my $styleFolder=$tree->style("create", "sf");
 	my $styleClassicFolder=$tree->style("create", "scf");
 	my $styleMyComputerFolder = $tree->style("create", "srf");
 	
 	
 	#my $style2=$tree->style("create", "s2");
+	$tree->style('elements', $styleFolder, 'elemImg folderTxt');
 	$tree->style('elements', $styleClassicFolder, 'classicFolderImage folderTxt');
 	$tree->style('elements', $styleMyComputerFolder, 'myComputerFolderImage folderTxt');
 	#$tree->style('elements', $style2, 'folderImg folderTxt');
@@ -70,7 +87,7 @@ sub init {
 	$tree->style("layout", $styleMyComputerFolder,  "folderTxt", -padx => '2 6', -squeeze=>'x', -expand=>'ns');
 
 	
-	$tree->column('configure', 'folderTag', -itemstyle => 'scf');
+	$tree->column('configure', 'folderTag', -itemstyle => 'sf');
 	
 	my $treeRoot = $tree->item("id", "root");
 	print "XXX: ".$treeRoot."\n";
@@ -85,33 +102,40 @@ sub init {
 	$tree->item("style", "set", $treeRoot, $column, $styleMyComputerFolder);
 	$tree->item("text", $treeRoot, "folderTag", "My Computer" );
 	foreach my $root (@roots) {
-		print "$root"."\n";
-		my $item=$tree->item("create", -button => 'yes');
-		$tree->item("collapse", $item);
-		$tree->item("lastchild", $treeRoot, $item);
-		$tree->item("text", $item, "folderTag", "$root" );
-		my $itemC1=$tree->item("create", -button => 'no');
-		$tree->item("collapse", $itemC1);
-		$tree->item("lastchild", $item, $itemC1);
-		$tree->item("text", $itemC1, "folderTag", "itemC2" );
-		my $itemC2=$tree->item("create", -button => 'no');
-		$tree->item("collapse", $itemC2);
-		$tree->item("lastchild", $item, $itemC2);
-		$tree->item("text", $itemC2, "folderTag", "itemC2" );
-		my $itemC3=$tree->item("create", -button => 'no');
-		$tree->item("collapse", $itemC3);
-		$tree->item("lastchild", $item, $itemC3);
-		$tree->item("text", $itemC3, "folderTag", "itemC3" );	}
+		
+		$self->_addFolder($treeRoot, $root);
+
+#		print "$root"."\n";
+#		my $item=$tree->item("create", -button => 'yes');
+#		$tree->item("state", "set", $item, "hasMediaFile");
+#		$tree->item("collapse", $item);
+#		$tree->item("lastchild", $treeRoot, $item);
+#		$tree->item("text", $item, "folderTag", "$root" );
+#		$self->_addFolder($item, $root);
+
+#		my $itemC1=$tree->item("create", -button => 'no');
+#		$tree->item("collapse", $itemC1);
+#		$tree->item("lastchild", $item, $itemC1);
+#		$tree->item("text", $itemC1, "folderTag", "itemC1" );
+#		my $itemC2=$tree->item("create", -button => 'no');
+#		$tree->item("collapse", $itemC2);
+#		$tree->item("lastchild", $item, $itemC2);
+#		$tree->item("text", $itemC2, "folderTag", "itemC2" );
+#		my $itemC3=$tree->item("create", -button => 'no');
+#		$tree->item("collapse", $itemC3);
+#		$tree->item("lastchild", $item, $itemC3);
+#		$tree->item("text", $itemC3, "folderTag", "itemC3" );	
+	}
 	
 	$tree->notify("bind", $tree, "<Expand-before>", [
        sub {
        	   my $t = shift;
            my $i = shift;
-           my $item=$tree->item("create", -button => 'yes');
-				#$tree->item("collapse", $item);
-				$tree->item("text", $item, "folderTag", "TEST" );           
-				$tree->item("lastchild", $i, $item);
-				$tree->item("collapse", $item);
+#           my $item=$tree->item("create", -button => 'yes');
+#				#$tree->item("collapse", $item);
+#				$tree->item("text", $item, "folderTag", "TEST" );           
+#				$tree->item("lastchild", $i, $item);
+#				$tree->item("collapse", $item);
           # print "Clicked at $i\n";
           #Tkx::update("idletasks");
           return $i;
@@ -142,6 +166,117 @@ sub init {
 #	print $tree->item("create", -count => 5);	
 }
 
+# create a folder item (for tktreectrl) from the full dirname
+# create it folder or not, and whith the good item tag 
+# (music dir, mda.xml dir or normal dir)
+sub _createFolderItem {
+	my $self = shift;
+	my $parentItem = shift;
+	unless(defined $parentItem){
+		ERROR("Missing input parameter: directory name");
+		die;
+	}
+	
+}
+
+sub _addFolder {
+	my $self = shift;
+	my $parentItem = shift;
+	my $folderPath = shift;
+	my $tree=$self->tree();
+	#$folderPath="\\";
+
+	unless(defined $parentItem){
+		ERROR("Missing input parameter: parent item");
+		die;
+	}
+
+	unless(defined $folderPath){
+		ERROR("Missing input parameter: folder path");
+		die;
+	}
+
+	unless (-d $folderPath) {
+		WARN("Parameter '$folderPath' isn't a folder");
+		#die;		
+	}
+
+# TODO: handle windows drive roots (floppy, hard drive, removable, disk, network)
+
+	my $dirFD;
+	opendir($dirFD, $folderPath) || ( ERROR("Cannot open directory") and return);
+
+#	my $item=$tree->item("create", -button => 'yes');
+#	$tree->item("state", "set", $item, "hasMediaFile");
+#	$tree->item("lastchild", $parentItem, $item);
+	# We have a real folder in parameter, so create the corresponding item
+	my $item=$tree->item("create");
+	# The text of the item is the name of the folder we're adding (or else, it could be a drive or a root)
+	my $itemText = ( (File::Spec->splitdir( $folderPath ))[-1]);
+	if ($itemText =~ /^$/) {
+		$itemText = ( (File::Spec->splitdir( $folderPath ))[-2]);
+	}
+
+
+	# Examine each entry in this folder to know how to display it
+	foreach my $fileInDir (File::Spec->no_upwards(readdir($dirFD))) {
+		my $fileInDirFullPath=File::Spec->catfile($folderPath, $fileInDir);
+
+		# items has subFolders, so make it expandable
+		if(-d $fileInDirFullPath) {
+			$tree->item("configure", $item, -button => 'yes');
+			$tree->item("collapse", $item);
+			$tree->item("state", "set", $item, "hasSubFolders");
+			DEBUG ("Found directory '$fileInDirFullPath'\n");
+#			$self->_createFolderItem;
+		}elsif(-f $fileInDirFullPath) {
+			print("File: ");
+			DEBUG ("Found file '$fileInDirFullPath'\n");
+# TODO: escape dot in extension check (media extension .mp3 is used with dot unescaped)
+			foreach my $mediaExtension ( @$mediaExtensions ) {
+				print("trying extension $mediaExtension\n");
+				if($fileInDir =~ /^.*${mediaExtension}$/) {
+					DEBUG("$folderPath contain $fileInDir media file");
+					$tree->item("state", "set", $item, "hasMediaFile");
+				}
+				elsif($fileInDir =~ /^\.?mda.xml$/) {
+					DEBUG("$folderPath contain '$fileInDir' MDA XML file");
+					$tree->item("state", "set", $item, "hasMdaXmlFile");
+				}
+			}
+		}else {
+			WARN ("Directory entry '$fileInDirFullPath' isn't a directory nor a normal file\n");
+		}
+	}
+	$tree->item("text", $item, "folderTag", $itemText );
+	$tree->item("lastchild", $parentItem, $item);
+#	my $iter = File::Next::dirs( { file_filter => sub { print "File: ".$_."\n"; return 1; }, descend_filter => sub{print "Dir: ".$_."\n"; return 0} }, $folderPath );
+#    while ( defined ( my $file = $iter->() ) ) {
+#        # do something...
+#        print $File::Next::dir." XX $file\n";
+#    }
+#my($f) = File::Util->new();
+#foreach my $entry ($f->list_dir( $folderPath,'--no-fsdots' )) {
+#	print("$entry  \n");
+#}	
+print("XXXXXXXXXXXXXXXXXXX\n");
+# Check the folder properties: 
+# does it contains other folders
+
+# does it contains music files
+# does it contains .mda.xml
+
+# if folder contains other folders, add a button
+
+# if folder contains .mda.xml, use appli icon
+
+# if folder contains music, use music icon
+	
+#		my $item=$tree->item("create", -button => 'yes');
+#		$tree->item("collapse", $item);
+#		$tree->item("lastchild", $treeRoot, $item);
+#		$tree->item("text", $item, "folderTag", "$root" );
+}
 # Add parentWindow to object and create the tree associated with it
 sub parentWindow {
 	my $self = shift;
